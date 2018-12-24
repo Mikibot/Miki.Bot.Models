ba7dfd1b0534d2fc673ffea5c453a80782aeb822
Veld <velddev@gmail.com>
2018-12-24 07:02:21 +0100

    Add: APIToken

---
+++ Models/Authorization/APIToken.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Miki.Bot.Models.Models.Authorization
+{
+    public class APIToken
+    {
+        public ulong UserId { get; set; }
+        public DateTime LastTimeReset { get; set; }
+    }
+}
