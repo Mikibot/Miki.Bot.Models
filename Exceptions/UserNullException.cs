20cdd55d09218996f3b18627773cdb6dc9bc298c
Veld <velddev@gmail.com>
2018-12-25 20:20:16 +0100

    Add: UserNullException

---
+++ Exceptions/UserNullException.cs
@@ -0,0 +1,14 @@
+﻿using Miki.Localization;
+using Miki.Localization.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Miki.Bot.Models.Exceptions
+{
+	public class UserNullException : LocalizedException
+	{
+		public override IResource LocaleResource
+			=> new LanguageResource("error_user_null");
+	}
+}
