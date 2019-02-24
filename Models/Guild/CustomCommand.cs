44196b88b481ce09f684a990f871e6b36a1fa651
Veld <velddev@gmail.com>
2019-02-24 16:50:28 +0100

    Added UserLog model, CustomCommand model

---
+++ Models/Guild/CustomCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Miki.Bot.Models
+{
+    public class CustomCommand
+    {
+        public long guildId { get; set; }
+
+        public string commandName { get; set; }
+
+        public string commandBody { get; set; }
+    }
+}
