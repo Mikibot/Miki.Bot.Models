7770d60fb34dcafcc2f0c964e74c376ae2570864
Veld <velddev@gmail.com>
2019-08-11 04:56:17 +0200

    Add: scope migrations

---
+++ Migrations/20190811010740_ScopesAdded.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Miki.Migrations
+{
+    public partial class ScopesAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Scopes",
+                schema: "dbo",
+                columns: table => new
+                {
+                    UserId = table.Column<long>(nullable: false),
+                    ScopeId = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Scopes", x => new { x.ScopeId, x.UserId });
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Scopes",
+                schema: "dbo");
+        }
+    }
+}
