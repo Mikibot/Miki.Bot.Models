e8feba031459e4a406e605a7d4cf161f91da6f5a
Mike <veldsinkie.mv@gmail.com>
2019-08-25 20:34:35 +0200

    Add getters to AchievementRepository

--- Repositories/AchievementRepository.cs
+++ Repositories/AchievementRepository.cs
@@ -1,17 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Miki.Bot.Models.Repositories
 {
     public class AchievementRepository : IAsyncRepository<Achievement>
     {
+        DbSet<Achievement> set;
 
+        public AchievementRepository(DbContext ctx)
+        {
+            set = ctx.Set<Achievement>();
+        }
 
+        public Task<Achievement> GetAsync(long userId, string name)
+        {
+            return set.Where(x => x.Name == name
+                           && x.UserId == userId)
+                .OrderByDescending(x => x.Rank)
+                .FirstOrDefaultAsync();
+        }
         public Task<Achievement> GetAsync(params object[] id)
         {
-
+            if (id.Length != 2)
+            {
+                throw new ArgumentOutOfRangeException(nameof(id));
+            }
+            return GetAsync((long)id[0], (string)id[1]);
         }
 
         public Task AddAsync(Achievement entity)
