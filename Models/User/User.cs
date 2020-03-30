0cd50a68ae9f369f30ed94c2a90068655724940b
Veld <veldsinkie.mv@gmail.com>
2020-03-30 03:20:20 +0200

    Add User Entity Attribute

--- Models/User/User.cs
+++ Models/User/User.cs
@@ -7,8 +7,9 @@
     using System.Linq;
     using System.Threading.Tasks;
     using Microsoft.EntityFrameworkCore;
-    using Miki.Bot.Models.Queries;
+    using Miki.Bot.Models.Attributes;
 
+	[Entity("user")]
     [DataContract]
 	public class User
 	{
